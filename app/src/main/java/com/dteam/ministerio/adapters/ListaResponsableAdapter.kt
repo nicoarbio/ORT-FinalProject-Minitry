75f475806236343fbf074b66bc4794ad96b5176d
Alan Gao <1982548075@qq.com>
2021-10-31 17:24:24 -0300

    Agregado de lista responsable y adpater(Sin logica)

---
+++ app/src/main/java/com/dteam/ministerio/adapters/ListaResponsableAdapter.kt
@@ -0,0 +1,4 @@
+package com.dteam.ministerio.adapters
+
+class ListaResponsableAdapter {
+}
---
+++ app/src/main/java/com/dteam/ministerio/fragments/ResponsableList.kt
@@ -0,0 +1,30 @@
+package com.dteam.ministerio.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dteam.ministerio.R
+
+class ResponsableList : Fragment() {
+
+    companion object {
+        fun newInstance() = ResponsableList()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.responsable_list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        //viewModel = ViewModelProvider(this).get(ResponsableListViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
