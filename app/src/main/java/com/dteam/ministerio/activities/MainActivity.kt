4288b53e3e4c2ef88c8e204c968b1dd381031b9f
DamianMartinezz <80834828+DamianMartinezz@users.noreply.github.com>
2021-11-14 16:23:56 -0300

    BottomBar

--- app/src/main/java/com/dteam/ministerio/activities/MainActivity.kt
+++ app/src/main/java/com/dteam/ministerio/activities/MainActivity.kt
@@ -2,11 +2,21 @@ package com.dteam.ministerio.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.dteam.ministerio.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavView : BottomNavigationView
+    private lateinit var navHostFragment : NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+
+        bottomNavView = findViewById(R.id.bottomNav)
+
+        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
     }
 }
